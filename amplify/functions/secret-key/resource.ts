23d3bba0f3d2dac7b31a97e0d09f8c5cdd312394
Mustafa Yavuz Okumuş <47111280+myavuzokumus@users.noreply.github.com>
2025-04-12 20:01:48 +0300

    aws library added.

---
+++ amplify/functions/secret-key/resource.ts
@@ -0,0 +1,66 @@
+import { execSync } from "node:child_process";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineFunction } from "@aws-amplify/backend";
+import { DockerImage, Duration } from "aws-cdk-lib";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+
+const functionDir = path.dirname(fileURLToPath(import.meta.url));
+const isWindows = process.platform === 'win32';
+
+export const secretKeyFunctionHandler = defineFunction(
+  (scope) => {
+    const lambdaFunction = new Function(scope, "secret-key", {
+      handler: "index.handler",
+      runtime: Runtime.PYTHON_3_12,
+      timeout: Duration.seconds(20),
+        code: Code.fromAsset(functionDir, {
+          bundling: {
+            image: DockerImage.fromRegistry("public.ecr.aws/lambda/python:3.12"),
+            local: {
+              tryBundle(outputDir: string) {
+                const pythonCmd = isWindows ? 'python3.12' : 'python';
+
+                execSync(
+                  `${pythonCmd} -m pip install -r ${path.join(functionDir, "requirements.txt")} -t ${path.join(outputDir)} --platform manylinux2014_x86_64 --only-binary=:all:`
+                );
+
+                if (isWindows) {
+                  const sourceDir = functionDir + (functionDir.endsWith('\\') ? '' : '\\');
+                  execSync(`xcopy "${sourceDir}*" "${outputDir}" /E /I /Y`);
+                } else {
+                  execSync(`cp -r ${functionDir}/* ${outputDir}`);
+                }
+
+                return true;
+              },
+            },
+          },
+        }),
+    });
+
+    // SSM Parameter Store erişim izinleri
+    lambdaFunction.addToRolePolicy(new PolicyStatement({
+      actions: [
+        'ssm:GetParameter',
+        'ssm:GetParameters'
+      ],
+      resources: [
+        "arn:aws:ssm:eu-central-1:471112835770:parameter/qr-generator/*"
+      ]
+    }));
+
+    // IoT Core yayın izinleri
+    lambdaFunction.addToRolePolicy(new PolicyStatement({
+      actions: [
+        'iot:Publish'
+      ],
+      resources: [
+        "arn:aws:iot:eu-central-1:471112835770:topic/room/*"
+      ]
+    }));
+
+    return lambdaFunction;
+  }
+);
