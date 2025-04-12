ad68ad3f669d30600c450c197119b826e637c603
Mustafa Yavuz Okumuş <47111280+myavuzokumus@users.noreply.github.com>
2025-04-12 20:05:26 +0300

    Add new function handlers for user preferences, secret keys, event data, sensor data, and room control

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,6 +3,11 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { verifyQrFunctionHandler } from './functions/verify-qr/resource';
 import { aiAgentFunctionHandler } from './functions/ai-agent/resource';
+import { userPreferencesFunctionHandler } from './functions/user-pref/resource';
+import { secretKeyFunctionHandler } from './functions/secret-key/resource';
+import { requestEventDataFunctionHandler } from './functions/request-event-data/resource';
+import { requestSensorDataFunctionHandler } from './functions/request-sensor-data/resource';
+import { requestRoomControlFunctionHandler } from './functions/request-room-control/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -12,6 +17,11 @@ defineBackend({
   data,
   verifyQrFunctionHandler,
   aiAgentFunctionHandler,
+  userPreferencesFunctionHandler,
+  secretKeyFunctionHandler,
+  requestEventDataFunctionHandler,
+  requestSensorDataFunctionHandler,
+  requestRoomControlFunctionHandler
 });
 
 // Add your custom resources here
